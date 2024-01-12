c07cb27246585e42fb22479c9d028243890a42c1
m1xture <109725502+m1xture@users.noreply.github.com>
2024-01-12 22:34:06 +0200

    мы обеспечили себе лучшие выходные
    echo + ping

--- src/index.js
+++ src/index.js
@@ -1,30 +1,115 @@
-require('dotenv').config()
-const { Client, IntentsBitField } = require('discord.js');
-const mongoose = require('mongoose');
-const eventHandler = require('./handlers/eventHandler');
+import {
+  Client,
+  GatewayIntentBits,
+  Guild,
+  Routes,
+  Events,
+  Collection,
+} from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { config } from "dotenv";
+import { REST } from "@discordjs/rest";
+import { commands } from "./commands/ping.js";
+// import { commands } from "./getAllFiles";
 
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildPresences,
-        IntentsBitField.Flags.MessageContent,
-    ],
+// const fs = require("node:fs");
+// const path = require("node:path");
+
+//? import fs from "node:fs";
+//? import path from "node:path";
+//? import { fileURLToPath } from "url";
+
+config();
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+export const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-const startBot = (async () => {
-    try {
-        mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Connected to DB");
-    } catch (error) {
-        console.log(`Error: ${error}`);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+//? lala
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// client.commands = new Collection();
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// const foldersPath = path.join("../", "commands");
+// const commandFolders = fs.readdirSync(foldersPath);
+
+// for (const folder of commandFolders) {
+//   const commandsPath = path.join(foldersPath, folder);
+//   const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
+//     // Set a new item in the Collection with the key as the command name and the value as the exported module
+//     if ("data" in command && "execute" in command) {
+//       client.commands.set(command.data.name, command);
+//     } else {
+//       console.log(
+//         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+//       );
+//     }
+//   }
+// }
+
+//? al;a
+
+client.on("ready", () => console.log("I am ready to use"));
+
+client.on(Events.InteractionCreate, (interaction) => {
+  console.log(interaction);
+  if (interaction.isChatInputCommand()) {
+    switch (interaction.commandName) {
+      case "ping":
+        interaction.reply({
+          content: "pong",
+        });
+
+        return;
+
+      default:
+        break;
     }
+  }
+  // interaction.reply({
+  //     content:
+  // })
+});
 
-    eventHandler(client)
-    
-    client.login(process.env.TOKEN);
-})
+client.on(Events.MessageCreate, (msg) => {
+  console.log(msg);
+  try {
+    if (!msg.author.bot) {
+      msg.channel.send(msg.content);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-startBot();
+async function main() {
+  try {
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    client.login(TOKEN);
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
--- src/utils/calculateLevelXp.js
+++
@@ -1,1 +0,0 @@
-module.exports = (level) => 100 * level || 1;
