bfa1eaa5ad114093bf9340d381e518e401f2d613
Olexander1234 <105589389+Olexander1234@users.noreply.github.com>
2024-01-22 20:09:02 +0200

    Update index.js
    зробив шо якшо в проміжку 60 секунд юзер відправив 3 однакових повідомлень то йому змішується xp а з фото просто якшо воно було надіслане то також зменшення xp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const {
-  Client,
-  GatewayIntentBits,
-  Guild,
-  Routes,
-  Events,
-  Collection,
+    Client,
+    GatewayIntentBits,
+    Guild,
+    Routes,
+    Events,
+    Collection,
 } = require("discord.js");
 const { config } = require("dotenv");
 const accrualPoints = require("./utils/messages.js");
@@ -30,13 +30,13 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const DATABASE_NAME = "messages";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildVoiceStates,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
 });
 
 // const rest = new REST({ version: "10" }).setToken(TOKEN);
@@ -47,73 +47,73 @@ const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
+        }
     }
-  }
 }
 
-client.on("ready", async () => {
-  console.log(`${client.user.tag} is online`);
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to DB");
-  } catch (error) {
-    console.log(`There was an error whil connecting to database ${error}`);
-  }
+client.on("ready", async() => {
+    console.log(`${client.user.tag} is online`);
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to DB");
+    } catch (error) {
+        console.log(`There was an error whil connecting to database ${error}`);
+    }
 });
 
 // <<<<<<< top
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const currentUsers = await Level.find({ userId: interaction.user.id });
-  // console.log(currentUsers)
+client.on(Events.InteractionCreate, async(interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    const currentUsers = await Level.find({ userId: interaction.user.id });
+    // console.log(currentUsers)
 
-  if (currentUsers.length === 0) {
-    const newUser = new Level({
-      userId: interaction.user.id,
-      guildId: interaction.guild.id,
-      xp: 0,
-      level: 1,
-    });
-    console.log("some");
-    // console.log(await newUser.save())
-    await newUser.save();
-  }
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found`);
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+    if (currentUsers.length === 0) {
+        const newUser = new Level({
+            userId: interaction.user.id,
+            guildId: interaction.guild.id,
+            xp: 0,
+            level: 1,
+        });
+        console.log("some");
+        // console.log(await newUser.save())
+        await newUser.save();
     }
-  }
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found`);
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        }
+    }
 });
 
 //todo: Обробник подій який додає бали за повідомлення довші за 3 літери
@@ -121,16 +121,15 @@ client.on("messageCreate", accrualPoints);
 
 //todo: Nitro boost event
 client.on(Events.GuildMemberUpdate, (oldMember, newMember) => {
-  if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
-    if (
-      !oldMember.roles.cache.has("1192072016866574386") &&
-      newMember.roles.cache.has("1192072016866574386")
-    ) {
-      client.guilds.cache
-        .first()
-        .systemChannel.send(`Boost event by <@${newMember.user.id}>`);
+    if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
+        if (!oldMember.roles.cache.has("1192072016866574386") &&
+            newMember.roles.cache.has("1192072016866574386")
+        ) {
+            client.guilds.cache
+                .first()
+                .systemChannel.send(`Boost event by <@${newMember.user.id}>`);
+        }
     }
-  }
 });
 
 // todo: Обробник подій який додає бали якщо у голосову чаті присутні щонайменше четверо осіб
@@ -156,61 +155,88 @@ const messages = require("./models/messages.js");
 
 // ...
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
+const spamCooldown = 60000; // період часу для визначення спаму в мілісекундах (тут 60 секунд)
+const maxSameMessages = 3; // максимальна кількість однакових повідомлень для спаму
 
-  const userId = message.author.id;
-  const content = message.content;
+const userCooldowns = new Map();
 
-  try {
-    // Збереження нового повідомлення в базу даних
-    const newMessage = new messages({
-      userId: userId,
-      message: content,
-    });
-    await newMessage.save();
-    console.log(`Користувач написав: ${content}`);
+client.on("messageCreate", async(message) => {
+    if (message.author.bot) return;
 
-    // Пошук кількості однакових повідомлень від цього користувача
-    const countOfSameMessages = await messages.countDocuments({
-      userId: userId,
-      message: content,
-    });
+    const userId = message.author.id;
+    const content = message.content;
 
-    // Якщо кількість однакових повідомлень більше трьох, вивести повідомлення
-    if (countOfSameMessages > 3) {
-      console.log(
-        `Користувач ${userId} відправив ${countOfSameMessages} однакових повідомлення: ${content}`
-      );
-      message.reply(
-        `Ви відправили ${countOfSameMessages} однакових повідомлення. Будь ласка, утримайте себе від повторень.`
-      );
+    try {
+        // Збереження нового повідомлення в базу даних
+        const newMessage = new messages({
+            userId: userId,
+            message: content,
+        });
+        await newMessage.save();
+        console.log(`Користувач написав: ${content}`);
+
+        const countOfSameMessages = await messages.countDocuments({
+            userId: userId,
+            message: content,
+        });
+
+        // Перевірка на спам за інтервалом часу
+        if (userCooldowns.has(userId)) {
+            const timeDiff = Date.now() - userCooldowns.get(userId);
+            if (timeDiff < spamCooldown && countOfSameMessages >= maxSameMessages) {
+                // Якщо користувач відправляє більше 3 однакових повідомлень за короткий інтервал часу, зменшуємо його досвід
+                const userLevel = await Level.findOne({ userId });
+                if (userLevel !== null) {
+                    const exp = userLevel.xp - 1;
+                    await Level.updateOne({ userId: userId }, { xp: exp });
+                    console.log(`Зменшено досвід користувача ${userId} через спам.`);
+                    message.reply(
+                        `Ви відправили ${countOfSameMessages} однакових повідомлень. Зменшено XP через спам`
+                    );
+                }
+                return;
+            }
+        }
+
+
+
+        // Оновлюємо час останнього відправленого повідомлення для користувача
+        userCooldowns.set(userId, Date.now());
+    } catch (error) {
+        console.error("Помилка при збереженні:", error);
     }
-  } catch (error) {
-    console.error("Помилка при збереженні:", error);
-  }
 });
+client.on("messageCreate", async(message) => {
 
-client.on("messageCreate", async (message) => {
-  // Перевірка, чи автор повідомлення - бот або чи є текстовий контент
-  if (message.author.bot || !message.content) return;
+    if (message.author.bot || !message.content) return;
 
-  // Перевірка, чи є прикріплені файли
-  if (message.attachments.size > 0) {
-    // Якщо є фото, відправте відповідь
-    message.reply("вам не нараховано очок");
-  }
+    // Перевірка, чи є прикріплені файли (фото)
+    if (message.attachments.size > 0) {
+
+        message.reply("Вам не нараховано XP через відправлене фото.");
+
+        const userId = message.author.id;
+        const userLevel = await Level.findOne({ userId });
+        if (userLevel !== null) {
+            const exp = userLevel.xp - 1;
+            await Level.updateOne({ userId: userId }, { xp: exp });
+            console.log(`Зменшено XP користувача ${userId} через відправлене фото.`);
+        }
+
+        return;
+    }
+
+
 });
-
 client.on("messageDelete", async msg => {
-  const id = msg.author.id;
-  Level.findOne({ userId: id })
-    .exec()
-    .then((op) => {
-      if (op !== null) {
-        let exp = op.xp-1;
-        Level.updateOne({ userId: id }, { xp: exp }).then();
-      }
-    });
+    const id = msg.author.id;
+    Level.findOne({ userId: id })
+        .exec()
+        .then((op) => {
+            if (op !== null) {
+                let exp = op.xp - 1;
+                Level.updateOne({ userId: id }, { xp: exp }).then();
+            }
+        });
 });
 client.login(TOKEN);
