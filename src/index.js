f71b112c07be786f742bf3a86c9846dcc82d2371
EgorMaz230 <115287102+EgorMaz230@users.noreply.github.com>
2024-01-24 15:02:50 +0100

    Логіка для bad words

--- src/index.js
+++ src/index.js
@@ -255,16 +255,17 @@ client.on("messageCreate", async(message) => {
     if (message.attachments.size > 0) {
 
 
-client.on("messageDelete", async (msg) => {
-  const id = msg.author.id;
-  Level.findOne({ userId: id })
-    .exec()
-    .then((op) => {
-      if (op !== null) {
-        let exp = op.xp - 1;
-        Level.updateOne({ userId: id }, { xp: exp }).then();
-      }
-    });
+        client.on("messageDelete", async msg => {
+            const id = msg.author.id;
+            Level.findOne({ userId: id })
+                .exec()
+                .then((op) => {
+                    if (op !== null) {
+                        let exp = op.xp - 1;
+                        Level.updateOne({ userId: id }, { xp: exp }).then();
+                    }
+                });
+        });
 
         message.reply("Вам не нараховано XP через відправлене фото.");
 
@@ -293,4 +294,55 @@ client.on("messageDelete", async msg => {
         });
 
 });
+
+
+
+//////////////////////////////////////////////////////////////////////////// Egor code
+
+
+const badWords = ['bad_word1', 'bad_word2']; 
+const mutedUsers = new Map(); 
+const muteDuration = 12000; // 10 min
+
+client.on('messageCreate', async (message) => { 
+    if (message.author.bot) return; // Ignore messages from bots 
+
+    const userId = message.author.id; 
+
+    // Check if the user is currently muted 
+    if (mutedUsers.has(userId)) { 
+        const unmuteTime = mutedUsers.get(userId); 
+        if (unmuteTime > Date.now()) { 
+            await message.reply("Ви ще маєте діючий м'ют. Будь ласка, залишайтеся відсутнім(ою) від нецензурних слів."); 
+            await message.delete(); 
+            return; 
+        } 
+    } 
+
+    // Check for bad words 
+    let containsBadWord = false; 
+    for (const badWord of badWords) { 
+        if (message.content.toLowerCase().includes(badWord)) { 
+            containsBadWord = true; 
+            break; 
+        } 
+    } 
+
+    if (containsBadWord) { 
+        await message.reply('Це повідомлення було видалено через те, що воно містить нецензурне слово. Будь ласка, утримайтеся від використання такої мови.'); 
+        await message.delete(); 
+
+        const violations = (mutedUsers.get(userId) || 0) + 1; 
+        if (violations >= 2) { 
+            mutedUsers.set(userId, Date.now() + muteDuration); // Mute the user 
+            await message.channel.send(`Користувач @${message.author.tag} був замутений на ${(muteDuration / 60000).toFixed(2)} хвилин за використання нецензурної мови.`); 
+        } else { 
+            mutedUsers.set(userId, violations); 
+        } 
+        console.log(mutedUsers)
+
+    } 
+});
+
+//////////////////////////////////////////////////////////////////////////// Egor code
 client.login(TOKEN);
