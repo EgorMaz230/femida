347f6709816ad8e286550d804a539456e436e54c
m1xture <109725502+m1xture@users.noreply.github.com>
2024-03-19 21:20:50 +0200

    leaders cmd: pagination

--- src/commands/slashCommands/leaders.js
+++ src/commands/slashCommands/leaders.js
@@ -1,30 +1,127 @@
-const { SlashCommandBuilder } = require("discord.js");
-const { AttachmentBuilder } = require("discord.js");
-const creatingRatingEmbed = require("../../utils/creatingRatingEmbed");
+const {
+  SlashCommandBuilder,
+  AttachmentBuilder,
+  EmbedBuilder,
+} = require("discord.js");
+const Level = require("../../models/Level");
+const getUsersByIds = require("../../utils/getUsersByIds");
+const paginationFn = require("../../utils/pagination");
+
+async function sendError(interaction, description) {
+  const errorEmbed = new EmbedBuilder()
+    .setTitle("Помилка")
+    .setColor("#D04848")
+    .setDescription(description)
+    .setThumbnail("attachment://catError.gif");
+  const attachments = [
+    new AttachmentBuilder("src/imgs/catError.gif", "catError.gif"),
+  ];
+  await interaction
+    .editReply({
+      files: attachments,
+      embeds: [errorEmbed],
+    })
+    .catch((err) => console.log(err));
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("leaders")
     .setDescription("Надсилає рейтинг учасників серверу"),
   async execute(interaction, client) {
-    await interaction.deferReply();
-    const sendRatingFn = async () => {
-      const ratingEmbed = await creatingRatingEmbed(client);
-      let attachments = [];
-      if (ratingEmbed.data.title === "Помилка") {
-        attachments = [
-          new AttachmentBuilder("src/imgs/catError.gif", "catError.gif"),
-        ];
-      } else {
-        ratingEmbed.data.title = "Рейтинг учасників серверу";
+    try {
+      await interaction.deferReply();
+      const usersData = await Level.find({});
+      if (usersData.length === 0) {
+        await sendError(
+          interaction,
+          "Трапилася дивна помилка. Даза даних пуста"
+        );
+        return;
       }
-      await interaction
-        .editReply({
-          files: attachments,
-          embeds: [ratingEmbed],
+      const usersIds = usersData.map((user) => user.userId);
+      const usersObjs = await getUsersByIds(usersIds, client);
+
+      const usersArrEmbed = usersData.map((user) => {
+        const userName = usersObjs.find(
+          (userObj) => userObj.id === user.userId
+        ).displayName;
+        return {
+          name: userName,
+          __level: user.level,
+          __xp: user.xp,
+          inline: true,
+        };
+      });
+      const sortedUsersArrEmbed = usersArrEmbed
+        .sort((firstUser, secondUser) => {
+          if (firstUser.__level !== secondUser.__level) {
+            return secondUser.__level - firstUser.__level;
+          }
+          return secondUser.__xp - firstUser.__xp;
         })
-        .catch((err) => console.log(err));
-    };
-    sendRatingFn();
+        .filter((user) => user.__xp >= 5 || user.__level > 1);
+
+      if (sortedUsersArrEmbed.length === 0) {
+        await sendError(
+          interaction,
+          "Трапилася дивна помилка. На цьому сервері ймовірно \n немає користувачів з XP більше 4"
+        );
+        return;
+      }
+
+      const fieldsArr = sortedUsersArrEmbed.reduce(
+        (acc, { name, __xp, __level }) => {
+          acc.push({
+            name: `#${acc.length + 1} ` + name.trim(),
+            value: `Рівень: \`${__level}\`  XP: \`${__xp}\``,
+          });
+          return acc;
+        },
+        []
+      );
+
+      let embedsArr = [];
+      const size = 10;
+      if (sortedUsersArrEmbed.length >= size) {
+        const subarrays = [];
+        for (let i = 0; i < Math.ceil(fieldsArr.length / size); i++) {
+          subarrays[i] = fieldsArr.slice(i * size, (i + 1) * size);
+        }
+
+        embedsArr = Promise.all(
+          subarrays.map(async (page) => {
+            return new EmbedBuilder()
+              .setTitle("Рейтинг учасників серверу")
+              .setColor("#FFD23F")
+              .addFields(...page)
+              .setThumbnail(
+                client.guilds.cache
+                  .get(await usersData[0].guildId)
+                  .iconURL({ dynamic: true })
+              );
+          })
+        );
+      } else {
+        embedsArr.push(
+          new EmbedBuilder()
+            .setTitle("Рейтинг учасників серверу")
+            .setColor("#FFD23F")
+            .addFields(...fieldsArr)
+            .setThumbnail(
+              client.guilds.cache
+                .get(await usersData[0].guildId)
+                .iconURL({ dynamic: true })
+            )
+        );
+      }
+      await paginationFn(interaction, await embedsArr);
+    } catch (err) {
+      await sendError(
+        interaction,
+        "Трапилася дуууже дивна помилка. \n За можливості зверніться до адміністрації серверу"
+      );
+      console.log(`[LEADERS CMD ERROR] ${err}`);
+    }
   },
 };
