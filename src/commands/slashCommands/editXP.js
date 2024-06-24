2e15358e98bf66cfed8c1d5ad3f5e54e6e8d01bd
Oleksandr1shkin <cmurtimur@gmail.com>
2024-06-24 13:39:06 +0300

    закоментував код який видавав помилку в індекс.js і додав айді ролі в eдіт xp

--- src/commands/slashCommands/editXP.js
+++ src/commands/slashCommands/editXP.js
@@ -3,110 +3,107 @@ const Level = require("../../models/Level");
 const updateLevel = require("../../utils/updateLevel");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("edit-xp")
-    .setDescription("Add or subtract xp to someone's.")
-    .addUserOption((option) =>
-      option
-        .setName("target-user")
-        .setDescription("The user whose xp you want to add or subtract")
-        .setRequired(true)
-    )
-    .addNumberOption((option) =>
-      option
-        .setName("xp")
-        .setDescription("How many XP do you want to add or subtract to user")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("mode")
-        .setDescription('Choose "add" to add XP or "subtract" to subtract XP')
-        .addChoices(
-          { name: "Add", value: "add" },
-          { name: "Subtract", value: "subtract" }
+    data: new SlashCommandBuilder()
+        .setName("edit-xp")
+        .setDescription("Add or subtract xp to someone's.")
+        .addUserOption((option) =>
+            option
+            .setName("target-user")
+            .setDescription("The user whose xp you want to add or subtract")
+            .setRequired(true)
         )
-        .setRequired(true)
-    ),
+        .addNumberOption((option) =>
+            option
+            .setName("xp")
+            .setDescription("How many XP do you want to add or subtract to user")
+            .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+            .setName("mode")
+            .setDescription('Choose "add" to add XP or "subtract" to subtract XP')
+            .addChoices({ name: "Add", value: "add" }, { name: "Subtract", value: "subtract" })
+            .setRequired(true)
+        ),
 
-  async execute(interaction) {
-    if (!interaction.inGuild()) {
-      interaction.reply("You can't run this command inside a server");
-      return;
-    }
+    async execute(interaction) {
+        if (!interaction.inGuild()) {
+            interaction.reply("You can't run this command inside a server");
+            return;
+        }
 
-    const allowedRoles = ["953717386224226385", "953795856308510760"];
+        const allowedRoles = ["953717386224226385", "953795856308510760", "1192066790717661245"];
 
-    const member = interaction.guild.members.cache.get(interaction.user.id);
+        const member = interaction.guild.members.cache.get(interaction.user.id);
 
-    const hasAllowedRole = member.roles.cache.some((role) =>
-      allowedRoles.includes(role.id)
-    );
+        const hasAllowedRole = member.roles.cache.some((role) =>
+            allowedRoles.includes(role.id)
+        );
 
-    if (!hasAllowedRole) {
-      interaction.reply("Тільки адміністрація має право використовувати цю команду🙃");
-      return;
-    }
+        if (!hasAllowedRole) {
+            interaction.reply("Тільки адміністрація має право використовувати цю команду🙃");
+            return;
+        }
 
-    await interaction.deferReply();
+        await interaction.deferReply();
 
-    const mentionedUserId = interaction.options.get("target-user")?.value;
-    const targetUserId = mentionedUserId || interaction.member.id;
-    const targetUserObj = await interaction.guild.members.fetch(targetUserId);
+        const mentionedUserId = interaction.options.get("target-user")?.value;
+        const targetUserId = mentionedUserId || interaction.member.id;
+        const targetUserObj = await interaction.guild.members.fetch(targetUserId);
 
-    let userInfo = await Level.findOne({
-      userId: targetUserId,
-      guildId: interaction.guild.id,
-    });
+        let userInfo = await Level.findOne({
+            userId: targetUserId,
+            guildId: interaction.guild.id,
+        });
 
-    if (!userInfo) {
-      const newUser = new Level({
-        userId: targetUserId,
-        guildId: interaction.guild.id,
-        xp: 0,
-        currentXp: 0,
-        level: 1,
-      });
+        if (!userInfo) {
+            const newUser = new Level({
+                userId: targetUserId,
+                guildId: interaction.guild.id,
+                xp: 0,
+                currentXp: 0,
+                level: 1,
+            });
 
-      await newUser.save();
-      userInfo = await Level.findOne({
-        userId: targetUserId,
-        guildId: interaction.guild.id,
-      });
-    }
-    const replyEmbed = new EmbedBuilder()
-      .setTitle("Операція прошла успішно")
-      .setColor("#FFD23F")
-      .setTimestamp();
-    if (interaction.options.get("mode").value === "add") {
-      const xpToAdd = interaction.options.get("xp").value;
-      userInfo.xp += xpToAdd;
-      await userInfo.save();
-      await updateLevel(userInfo, userInfo.userId);
-      const userNewXP = await Level.findOne({ userId: targetUserId });
-      await interaction.editReply({
-        embeds: [
-          replyEmbed.setDescription(
-            `\`${xpToAdd}\` XP було додано до ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
-          ),
-        ],
-      });
-    } else if (interaction.options.get("mode").value === "subtract") {
-      let xpToSubtract = interaction.options.get("xp").value;
-      if (xpToSubtract > userInfo.xp) {
-        xpToSubtract = userInfo.xp;
-      }
-      userInfo.xp -= xpToSubtract;
-      await userInfo.save();
-      await updateLevel(userInfo, userInfo.userId);
-      const userNewXP = await Level.findOne({ userId: targetUserId });
-      await interaction.editReply({
-        embeds: [
-          replyEmbed.setDescription(
-            `\`${xpToSubtract}\` XP було віднято у ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
-          ),
-        ],
-      });
-    }
-  },
+            await newUser.save();
+            userInfo = await Level.findOne({
+                userId: targetUserId,
+                guildId: interaction.guild.id,
+            });
+        }
+        const replyEmbed = new EmbedBuilder()
+            .setTitle("Операція прошла успішно")
+            .setColor("#FFD23F")
+            .setTimestamp();
+        if (interaction.options.get("mode").value === "add") {
+            const xpToAdd = interaction.options.get("xp").value;
+            userInfo.xp += xpToAdd;
+            await userInfo.save();
+            await updateLevel(userInfo, userInfo.userId);
+            const userNewXP = await Level.findOne({ userId: targetUserId });
+            await interaction.editReply({
+                embeds: [
+                    replyEmbed.setDescription(
+                        `\`${xpToAdd}\` XP було додано до ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
+                    ),
+                ],
+            });
+        } else if (interaction.options.get("mode").value === "subtract") {
+            let xpToSubtract = interaction.options.get("xp").value;
+            if (xpToSubtract > userInfo.xp) {
+                xpToSubtract = userInfo.xp;
+            }
+            userInfo.xp -= xpToSubtract;
+            await userInfo.save();
+            await updateLevel(userInfo, userInfo.userId);
+            const userNewXP = await Level.findOne({ userId: targetUserId });
+            await interaction.editReply({
+                embeds: [
+                    replyEmbed.setDescription(
+                        `\`${xpToSubtract}\` XP було віднято у ${targetUserObj.user.tag}. Новий XP користувача - \`${userNewXP.xp}\`. `
+                    ),
+                ],
+            });
+        }
+    },
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const {
-  Client,
-  GatewayIntentBits,
-  Partials,
-  Events,
-  Collection,
-  AuditLogEvent,
+    Client,
+    GatewayIntentBits,
+    Partials,
+    Events,
+    Collection,
+    AuditLogEvent,
 } = require("discord.js");
 const { config } = require("dotenv");
 const path = require("node:path");
@@ -33,15 +33,15 @@ const antiSpam = require("./constants/antiSpam.js");
 // ініціалізація клієнту
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildInvites,
-  ],
-  partials: [Partials.Channel],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildInvites,
+    ],
+    partials: [Partials.Channel],
 });
 
 // зчитування папок із слеш функціями
@@ -52,28 +52,28 @@ const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
+        }
     }
-  }
 }
 
 config();
 
-client.on("ready", async (op) => {
-  database(client);
-  fetchInvites(op, client);
+client.on("ready", async(op) => {
+    database(client);
+    // fetchInvites(op, client);
 });
 
 limitPoints();
@@ -85,38 +85,38 @@ const TOKEN = process.env.TOKEN;
 
 // взаємодія з юзером
 
-client.on("guildMemberAdd", async (person) => {
-  updateInvites(person, client);
+client.on("guildMemberAdd", async(person) => {
+    updateInvites(person, client);
 });
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  addNewMember(interaction);
+client.on(Events.InteractionCreate, async(interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    addNewMember(interaction);
 
-  getInteractionCommands(interaction, client);
+    getInteractionCommands(interaction, client);
 });
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
-  await addNewMember(false, message);
-  if (message.attachments.size > 0 && message.content === "") return;
-  await accrualPoints(message);
-  await useAntispam(message);
-  await badWords(message);
+client.on("messageCreate", async(message) => {
+    if (message.author.bot) return;
+    await addNewMember(false, message);
+    if (message.attachments.size > 0 && message.content === "") return;
+    await accrualPoints(message);
+    await useAntispam(message);
+    await badWords(message);
 });
 
-client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
-  whenBoost(oldMember, newMember, client);
+client.on(Events.GuildMemberUpdate, async(oldMember, newMember) => {
+    whenBoost(oldMember, newMember, client);
 });
 
-client.on("voiceStateUpdate", async (oldState, newState) => {
-  await addNewMember(false, false, newState);
-  await voiceStateUpdate(oldState, newState, client);
-  await checkRoleInVc(oldState, newState, client);
+client.on("voiceStateUpdate", async(oldState, newState) => {
+    await addNewMember(false, false, newState);
+    await voiceStateUpdate(oldState, newState, client);
+    await checkRoleInVc(oldState, newState, client);
 });
 
-client.on("messageDelete", async (msg) => {
-  whenMessageDelete(msg, AuditLogEvent, client);
+client.on("messageDelete", async(msg) => {
+    whenMessageDelete(msg, AuditLogEvent, client);
 });
 
 client.login(TOKEN);
