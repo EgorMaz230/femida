7a22c18061a5804dd94f6f436db5586163a6f4de
Oleksandr1shkin <cmurtimur@gmail.com>
2024-06-26 20:18:00 +0300

    пофіксив де що

--- src/utils/updateLevel.js
+++ src/utils/updateLevel.js
@@ -2,28 +2,32 @@ const { config } = require("dotenv");
 const Level = require("../models/Level");
 const sendLevelNotification = require("./sendLevelNotification");
 const sendDmMsg = require("./sendDmMsg");
+const addRoleLevel = require("./addRoleLevel");
+
 config();
 
 module.exports = async function updateLevel({ level, xp }, userId) {
-  let xpForCurrentLvl = 0;
-  for (let i = 0; i < level; i++) {
-    xpForCurrentLvl += 5 * Math.pow(i, 2) + 50 * i + 100;
-  }
-  let newLevel = 0;
-  function calculateXPForLevel(lvl) {
-    let xpForLevel = 0;
-    for (let i = 0; i < lvl; i++) {
-      xpForLevel += 5 * Math.pow(i, 2) + 50 * i + 100;
+    let xpForCurrentLvl = 0;
+    for (let i = 0; i < level; i++) {
+        xpForCurrentLvl += 5 * Math.pow(i, 2) + 50 * i + 100;
     }
-    return xpForLevel;
-  }
-  while (xp >= calculateXPForLevel(newLevel + 1)) {
-    newLevel++;
-  }
-  if (newLevel > level) {
-    await sendLevelNotification();
-    if (newLevel % 5 === 0) await sendDmMsg({ id: userId, level: newLevel });
-  }
-  await Level.findOneAndUpdate({ userId }, { level: newLevel });
-  return newLevel;
+    let newLevel = 0;
+
+    function calculateXPForLevel(lvl) {
+        let xpForLevel = 0;
+        for (let i = 0; i < lvl; i++) {
+            xpForLevel += 5 * Math.pow(i, 2) + 50 * i + 100;
+        }
+        return xpForLevel;
+    }
+    while (xp >= calculateXPForLevel(newLevel + 1)) {
+        newLevel++;
+    }
+    if (newLevel > level) {
+        await sendLevelNotification();
+        if (newLevel % 5 === 0) await sendDmMsg({ id: userId, level: newLevel });
+    }
+    await Level.findOneAndUpdate({ userId }, { level: newLevel });
+    await addRoleLevel({ level: newLevel, xp }, userId);
+    return newLevel;
 };
