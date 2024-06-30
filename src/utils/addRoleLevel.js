d13cf3ac4a64ea41d574bdf4a953af07898a4767
Oleksandr1shkin <cmurtimur@gmail.com>
2024-06-30 22:10:52 +0300

    Update addRoleLevel.js

--- src/utils/addRoleLevel.js
+++ src/utils/addRoleLevel.js
@@ -2,19 +2,38 @@ const { config } = require("dotenv");
 const Level = require("../models/Level");
 
 config();
+// тест ролі 
+// const roles = [
+//     { level: 0, roleId: "1255566866064212019" },
+//     { level: 5, roleId: '1253646059486580837' },
+//     { level: 10, roleId: '1253646156173672508' },
+//     { level: 15, roleId: '1253646191644901439' },
+//     { level: 20, roleId: '1253646251732238377' },
+//     { level: 25, roleId: '1253646314621632572' },
+//     { level: 30, roleId: '1253646399820795964' },
+//     { level: 35, roleId: '1254385618830491740' },
+//     { level: 40, roleId: '1254385756055670826' },
+//     { level: 45, roleId: '1254385850079379456' },
+//     { level: 50, roleId: '11254385910401863782' },
+// ];
 
+
+
+
+
+// Goiteens ролі
 const roles = [
-    { level: 0, roleId: "1255566866064212019" },
-    { level: 5, roleId: '1253646059486580837' },
-    { level: 10, roleId: '1253646156173672508' },
-    { level: 15, roleId: '1253646191644901439' },
-    { level: 20, roleId: '1253646251732238377' },
-    { level: 25, roleId: '1253646314621632572' },
-    { level: 30, roleId: '1253646399820795964' },
-    { level: 35, roleId: '1254385618830491740' },
-    { level: 40, roleId: '1254385756055670826' },
-    { level: 45, roleId: '1254385850079379456' },
-    { level: 50, roleId: '11254385910401863782' },
+    { level: 0, roleId: "967373815292264469" },
+    { level: 5, roleId: '972169804272263238' },
+    { level: 10, roleId: '970999855126298624' },
+    { level: 15, roleId: '972170463264530482' },
+    { level: 20, roleId: '972170979637882980' },
+    { level: 25, roleId: '971000630648930315' },
+    { level: 30, roleId: '972174829656629318' },
+    { level: 35, roleId: '972175023399923783' },
+    { level: 40, roleId: '972175178744348672' },
+    { level: 45, roleId: '972175521251209236' },
+    { level: 50, roleId: '971001223589298247' },
 ];
 
 const guildId = process.env.GUILD_ID;
