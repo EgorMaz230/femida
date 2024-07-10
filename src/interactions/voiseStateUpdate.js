b031b35cf6df5a8ff8a6bc8b819c8732e7ab3424
Oleksandr1shkin <cmurtimur@gmail.com>
2024-07-10 22:49:04 +0300

    зробив що Якщо користувач має роль студента то йому 2 рази більше нараховується xp за повідомлення і голосовий чат

--- src/interactions/voiseStateUpdate.js
+++ src/interactions/voiseStateUpdate.js
@@ -1,61 +1,70 @@
 const Level = require("../models/Level");
 const updateLevel = require("../utils/updateLevel");
 
-module.exports = async (oldState, newState, client) => {
-  if (newState.channelId) {
-    if (!oldState.channelId && newState.channelId) {
-      const voiceChannels = client.channels.cache.filter(
-        (elem) => elem.type === 2 || elem.type === 13
-      );
-      let stvoiceChannel = {};
-      await client.channels
-        .fetch(newState.channelId)
-        .then((channel) => (stvoiceChannel = channel))
-        .catch((err) => console.log(err));
+module.exports = async(oldState, newState, client) => {
+    const studentRoleId = "1260674858602467428"; // Роль 'student'
 
-      const arrObj = stvoiceChannel.members;
-      const chanelMembers = arrObj.map((member) => member.user.id);
-
-      async function fetchMembers() {
-        let voiceChannel = {};
-        await client.channels
-          .fetch(newState.channelId)
-          .then((channel) => (voiceChannel = channel))
-          .catch((err) => console.log(err));
-
-        const members = voiceChannel.members;
-
-        const userIds = members.map((member) => member.user.id);
-        userIds.forEach(async (user) => {
-          const people = await Level.findOne({ userId: user });
-          if (people.currentXp !== 150) {
-            let updateXp = people.currentXp + 20;
-            if (updateXp > 150) {
-              updateXp = 150;
-            }
-            await Level.findOneAndUpdate(
-              { userId: user },
-              { currentXp: updateXp }
+    if (newState.channelId) {
+        if (!oldState.channelId && newState.channelId) {
+            const voiceChannels = client.channels.cache.filter(
+                (elem) => elem.type === 2 || elem.type === 13
             );
-            await updateLevel(people, user);
-            // console.log("people", people);
-          }
-        });
-      }
+            let stvoiceChannel = {};
+            await client.channels
+                .fetch(newState.channelId)
+                .then((channel) => (stvoiceChannel = channel))
+                .catch((err) => console.log(err));
 
-      if (chanelMembers.length === 4) {
-        await fetchMembers();
-      }
+            const arrObj = stvoiceChannel.members;
+            const chanelMembers = arrObj.map((member) => member.user.id);
 
-      if (arrObj.length > 4) {
-        const people = await Level.findOne({ userId: newState.id });
-        const updateXp = people.xp + 20;
-        await Level.findOneAndUpdate(
-          { userId: newState.id },
-          { currentXp: updateXp }
-        );
-        await updateLevel(people, newState.id);
-      }
+            async function fetchMembers() {
+                let voiceChannel = {};
+                await client.channels
+                    .fetch(newState.channelId)
+                    .then((channel) => (voiceChannel = channel))
+                    .catch((err) => console.log(err));
+
+                const members = voiceChannel.members;
+
+                const userIds = members.map((member) => member.user.id);
+                userIds.forEach(async(user) => {
+                    const member = members.get(user);
+                    const people = await Level.findOne({ userId: user });
+                    let xpToAdd = 20;
+
+                    if (member.roles.cache.has(studentRoleId)) {
+                        xpToAdd *= 2; // Нараховуємо подвійну кількість XP для студентів
+                    }
+
+                    if (people.currentXp !== 150) {
+                        let updateXp = people.currentXp + xpToAdd;
+                        if (updateXp > 150) {
+                            updateXp = 150;
+                        }
+                        await Level.findOneAndUpdate({ userId: user }, { currentXp: updateXp });
+                        await updateLevel(people, user);
+                    }
+                });
+            }
+
+            if (chanelMembers.length === 4) {
+                await fetchMembers();
+            }
+
+            if (arrObj.length > 4) {
+                const member = arrObj.get(newState.id);
+                const people = await Level.findOne({ userId: newState.id });
+                let xpToAdd = 20;
+
+                if (member.roles.cache.has(studentRoleId)) {
+                    xpToAdd *= 2; // Нараховуємо подвійну кількість XP для студентів
+                }
+
+                const updateXp = people.currentXp + xpToAdd;
+                await Level.findOneAndUpdate({ userId: newState.id }, { currentXp: updateXp });
+                await updateLevel(people, newState.id);
+            }
+        }
     }
-  }
 };
